77687ad72cf98aba3870c5fd320085c1c2a8a10d
frlinw <matthieu.dambrune@gmail.com>
2022-05-20 17:38:20 +0000

    feature: add `isObject`

---
+++ src/isObject.js
@@ -0,0 +1,9 @@
+/**
+ * Check if a value is an object literal
+ * @param {any} value
+ * @return {boolean}
+ */
+export default function isObject (value) {
+  return value?.constructor === Object
+}
+  
