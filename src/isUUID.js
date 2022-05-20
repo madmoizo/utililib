d6dc293cae6d6a82bcdf0705b1f2f8fb699fd21c
frlinw <matthieu.dambrune@gmail.com>
2022-05-20 20:52:24 +0000

    feature: add `isUUID`

---
+++ src/isUUID.js
@@ -0,0 +1,8 @@
+/**
+ * Check if a string matchs the UUID format
+ * @param {string} value
+ * @returns {boolean}
+ */
+export default function isUUID (value) {
+  return /([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})/.test(value)
+}
