22dcf48b9920248064fd90f8b3ac44c1cb79505b
frlinw <matthieu.dambrune@gmail.com>
2022-05-20 17:39:47 +0000

    feature: add `isUrl`

---
+++ src/isUrl.js
@@ -0,0 +1,9 @@
+/**
+ * Check if a value is a URL
+ * @param {any} value
+ * @return {boolean}
+ */
+export default function isUrl (value) {
+  return /^https?:\/\/[a-zA-Z0-9-_.?=&]/.test(value)
+}
+  
