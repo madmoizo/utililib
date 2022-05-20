426952cb3c6e9319fe470734a4ac46e943863b52
frlinw <matthieu.dambrune@gmail.com>
2022-05-20 17:40:13 +0000

    feature: add `parseDate`

---
+++ src/parseDate.js
@@ -0,0 +1,9 @@
+/**
+ * Transform a string representation of a date into a javascript Date
+ * @param {string} dateString
+ * @return {Date}
+ */
+export default function parseDate (dateString) {
+  return new Date(dateString)
+}
+  
