7394715763cd021a1b342b04c473ac8e9c62a535
frlinw <matthieu.dambrune@gmail.com>
2022-05-20 18:15:26 +0000

    fix(isObjectEmpty): does not throw an error if not an object

--- src/isObjectEmpty.js
+++ src/isObjectEmpty.js
@@ -1,15 +1,9 @@
-import isObject from './isObject.js'
-
-
 /**
  * Check if an objet is empty
  * @param {object} value
  * @return {boolean}
  */
 export default function isObjectEmpty (obj) {
-  if (!isObject(obj)) {
-    throw new Error(`Not an object`)
-  }
   return Object.keys(obj).length === 0
 }
   
