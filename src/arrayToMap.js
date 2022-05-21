b2906d11fe14c9f08ccfdc4cb8bdacb1c3391f77
frlinw <matthieu.dambrune@gmail.com>
2022-05-21 18:52:24 +0000

    feature: add `arrayToMap`

---
+++ src/arrayToMap.js
@@ -0,0 +1,14 @@
+/**
+ * Convert an array of objects to a Map
+ * @param {Array<object>} arr
+ * @param {string} key
+ * @return {Map} hastable using key's value as identifier for each object of the array
+ */
+export default function arrayToMap (arr, key) {
+  const map = new Map()
+  for (const item of arr) {
+    map.set(item[key], item)
+  }
+  return map
+}
+  
