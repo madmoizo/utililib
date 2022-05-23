4b6093c5cc21cbc4a49c2c282c9e4edb466bf439
frlinw <matthieu.dambrune@gmail.com>
2022-05-23 21:31:39 +0000

    feature(has): support any value not only object

--- src/has.js
+++ src/has.js
@@ -1,11 +1,16 @@
+import isNullish from './isNullish.js'
+
+
 /**
  * Wrapper for object prototype hasOwnProperty method
  * because hasOwnProperty may be shadowed by properties on the object
- * @param {Object} object
- * @param {String} key - key to find in the object
+ * @param {any} value
+ * @param {string} key - key to find in the object
  * @returns {boolean}
  */
-export default function has (object, key) {
-  return Object.prototype.hasOwnProperty.call(object, key)
+export default function has (value, key) {
+  return isNullish(value)
+    ? false
+    : Object.prototype.hasOwnProperty.call(value, key)
 }
   
