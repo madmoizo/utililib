ea994fb0fd37613e008d6f68268ea2ab7f5ca5b7
frlinw <matthieu.dambrune@gmail.com>
2022-05-26 20:51:24 +0000

    feature: add `stringifySearchParams`

---
+++ src/stringifySearchParams.js
@@ -0,0 +1,26 @@
+/**
+ * Transform a dictionary into a stringified url searchParams
+ * @param {object} searchParams
+ * @returns {string}
+ */
+export default function stringifySearchParams (searchParams) {
+  let requestUrl = ''
+
+  if (searchParams) {
+    requestUrl = Object.entries(searchParams)
+      .filter(([key, value]) => value !== undefined)
+      .map(([key, value]) => {
+        if (isDate(value)) {
+          value = value.toISOString()
+        }
+        return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+      })
+      .join('&')
+  }
+  // Add leading '?'
+  if (requestUrl) {
+    requestUrl = `?${requestUrl}`
+  }
+
+  return requestUrl
+}
