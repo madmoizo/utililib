321c8907313e3732caf608f1bec384d89914e3bf
madmoizo <matthieu.dambrune@gmail.com>
2022-05-31 13:42:23 +0000

    refactor(kebabToCamel): fix jsdoc & arg name

--- src/kebabToCamel.js
+++ src/kebabToCamel.js
@@ -2,12 +2,12 @@ import capitalizeFirstLetter from './capitalizeFirstLetter.js'
 
 
 /**
- * Transform kebab-case string to camelCase
- * @param {string} str
+ * Transform kebab-case string to CamelCase
+ * @param {string} string
  * @returns {string}
  */
-export default function kebabToCamel (str) {
-  return str
+export default function kebabToCamel (string) {
+  return string
     .split('-')
     .reduce((accu, current) => `${accu}${capitalizeFirstLetter(current)}`, '')
 }
