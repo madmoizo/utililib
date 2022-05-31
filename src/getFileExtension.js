5fd733860245edb98671c2f81f89c570574d7a28
madmoizo <matthieu.dambrune@gmail.com>
2022-05-31 14:23:41 +0000

    refactor(getFileExtension): return `null` if the extension can't be found

--- src/getFileExtension.js
+++ src/getFileExtension.js
@@ -1,15 +1,15 @@
 /**
  * Retrieve the file extension from an URL or a base64 string
- * @param {string} src
- * @return {string}
+ * @param {string} file
+ * @return {string | null}
  */
-export default function getFileExtension (src) {
-  if (isUrl(src)) {
+export default function getFileExtension (file) {
+  if (isUrl(file)) {
     return src.split(/[#?]/)[0].split('.').pop().trim()
   }
-  if (isBase64(src)) {
-    return src.substring('data:image/'.length, src.indexOf(';base64'))
+  if (isBase64(file)) {
+    return file.substring('data:image/'.length, file.indexOf(';base64'))
   }
 
-  return ''
+  return null
 }
