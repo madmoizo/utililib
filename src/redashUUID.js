9a202edf0be93e3768035b1d5fbd5b6c652aac08
madmoizo <matthieu.dambrune@gmail.com>
2022-05-31 13:33:50 +0000

    refactor(redashUUID): consistent arg name

--- src/redashUUID.js
+++ src/redashUUID.js
@@ -1,8 +1,8 @@
 /**
  * Add dashes to a string with UUID format
- * @param {string} value
+ * @param {string} string
  * @returns {string}
  */
-export default function redashUUID (str) {
-  return str.replace(/([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})/, '$1-$2-$3-$4-$5')
+export default function redashUUID (string) {
+  return string.replace(/([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})/, '$1-$2-$3-$4-$5')
 }
