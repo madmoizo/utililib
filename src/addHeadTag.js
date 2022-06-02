a9a9b853ecbae6990a8a76b9e234a7d88bc636a5
madmoizo <matthieu.dambrune@gmail.com>
2022-06-02 23:17:42 +0000

    style(addHeadTag): add jsdoc

--- src/addHeadTag.js
+++ src/addHeadTag.js
@@ -1,5 +1,9 @@
 /**
  * Import script on runtime
+ * @param {string} tagName
+ * @param {object} attributes
+ * @param {function} onload
+ * @returns {HTMLElement}
  */
 export default function addHeadTag (tagName, attributes, onload = null) {
   // Do not add an already existing id
@@ -12,14 +16,14 @@ export default function addHeadTag (tagName, attributes, onload = null) {
 
   const el = document.createElement(tagName)
   document.head.appendChild(el)
- 
+
   if (onload) {
     el.addEventListener('load', onload)
   }
-  
+
   for (const attributeName in attributes) {
     el[attributeName] = attributes[attributeName]
   }
-  
+
   return el
 }
