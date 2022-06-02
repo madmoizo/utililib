12e211d1ae37fa7967fd6f7aa846fc4e60493b83
madmoizo <matthieu.dambrune@gmail.com>
2022-06-02 22:04:54 +0000

    refactor(has): remplace `isNullish` by a `try catch`

--- src/has.js
+++ src/has.js
@@ -1,14 +1,14 @@
-import isNullish from './isNullish.js'
-
-
 /**
  * Wrapper for object prototype hasOwnProperty method
  * because hasOwnProperty may be shadowed by properties on the object
  * @param {any} value
- * @param {string} key - key to find in the object
+ * @param {string} key - key to find in the value
  * @returns {boolean}
  */
 export default function has (value, key) {
-  return !isNullish(value) && Object.prototype.hasOwnProperty.call(value, key)
+  try { // nullish value throw an error
+    return Object.prototype.hasOwnProperty.call(value, key)
+  } catch {
+    return false
+  }
 }
-  
