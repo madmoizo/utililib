df9fa99e15b2fd9ca0260d6394a0fc7296d41842
madmoizo <matthieu.dambrune@gmail.com>
2022-06-22 23:04:38 +0000

    fix(VeryLastINdexOf): should return -1 if `search` not found

--- src/veryLastIndexOf.js
+++ src/veryLastIndexOf.js
@@ -1,9 +1,13 @@
 /**
- * Find the last index of the end of a string
+ * Find the index of the end of the last occurence of the value
  * @param {string} value
- * @param {string} searchString
+ * @param {string} search
  * @returns {number}
  */
-export default function veryLastIndexOf (value, searchString) {
-  return value.lastIndexOf(searchString) + searchString.length
+export default function veryLastIndexOf (value, search) {
+  let lastIndexOf = value.lastIndexOf(search)
+  if (lastIndexOf !== -1) {
+    lastIndexOf += search.length
+  }
+  return lastIndexOf
 }
