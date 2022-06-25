c43ead0ef15d9036301bf870d68ccd3ab3c6126d
madmoizo <matthieu.dambrune@gmail.com>
2022-06-25 03:24:35 +0000

    refactor(unaccent): modern syntax

--- src/unaccent.js
+++ src/unaccent.js
@@ -7,5 +7,5 @@ export default function unaccent (value) {
   // https://stackoverflow.com/a/37511463/4906701
   return value
     .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/\p{Diacritic}/gu, '')
 }
