8b9bdbc54cbe28bd46943a1e99697847d61d5097
madmoizo <matthieu.dambrune@gmail.com>
2022-06-29 02:48:07 +0000

    fix(getFileExtension): imports missing

--- src/getFileExtension.js
+++ src/getFileExtension.js
@@ -1,3 +1,8 @@
+import isUrl from './isUrl.js'
+import isBase64 from './isBase64.js'
+import getBase64Extension from './getBase64Extension.js'
+
+
 /**
  * Retrieve the file extension from an URL or a base64 string
  * @param {string} file
@@ -8,7 +13,7 @@ export default function getFileExtension (file) {
     return src.split(/[#?]/)[0].split('.').pop().trim()
   }
   if (isBase64(file)) {
-    return file.match(/^data:.+\/(.+);base64,/)[1]
+    return getBase64Extension(file)
   }
 
   return null
