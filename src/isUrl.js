75a58058a04bcf5a9934bbeb4ac11d2843133372
frlinw <matthieu.dambrune@gmail.com>
2022-07-02 23:55:22 +0000

    fix(isUrl): add hashtag

--- src/isUrl.js
+++ src/isUrl.js
@@ -4,6 +4,6 @@
  * @return {boolean}
  */
 export default function isUrl (value) {
-  return /^https?:\/\/[a-zA-Z0-9-_.?=&]/.test(value)
+  return /^https?:\/\/[a-zA-Z0-9-_.?=&#]/.test(value)
 }
   
